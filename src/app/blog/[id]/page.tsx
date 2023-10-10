551b3e757c4ab1067daa4191503b7bfd69b235a6
Marcello Nazar <marcellonazarb@hotmail.com>
2023-10-10 16:38:51 -0300

    add responsiveness

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,12 +1,6 @@
 import { fetchOneTest } from "@/utils/api";
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const post = await fetchOneTest(params.id);
-  return {
-    title: post.title,
-    description: post.desc,
-  };
-}
+
 
 export default async function Blog({ params }: { params: { id: string } }) {
   const teste = await fetchOneTest(params.id);
